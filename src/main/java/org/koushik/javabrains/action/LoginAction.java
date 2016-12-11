7d292f05bbab90ed9461b5aa842936691dad4bdb
Jacek135 <jac.poczatek@gmail.com>
2016-12-11 17:09:05 +0100

    Tutorial 13 - done

--- src/main/java/org/koushik/javabrains/action/LoginAction.java
+++ src/main/java/org/koushik/javabrains/action/LoginAction.java
@@ -1,13 +1,26 @@
 package org.koushik.javabrains.action;
 
-import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.lang3.StringUtils;
 
-public class LoginAction implements Action{
+public class LoginAction extends ActionSupport {
     private String userId;
     private String password;
 
-    public String execute(){
-        if(getUserId().equals("userId") && (getPassword().equals("password"))){
+    @Override
+    public void validate() {
+        if (StringUtils.isEmpty(getUserId())) {
+            System.out.println("User ID cannot be blank");
+            addFieldError(getUserId(), "User ID cannot be blank");
+        }
+        if (StringUtils.isEmpty(getPassword())) {
+            System.out.println("Password cannot be blank");
+            addFieldError(getPassword(), "Password cannot be blank");
+        }
+    }
+
+    public String execute() {
+        if (getUserId().equals("userId") && (getPassword().equals("password"))) {
             return SUCCESS;
         }
         return LOGIN;
